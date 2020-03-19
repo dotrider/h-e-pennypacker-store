af20c3918c70a84a6bb8979ecb7141aa6e301fb8
dotrider <56666461+dotrider@users.noreply.github.com>
2020-03-19 11:54:28 -0700

    controller set

---
+++ server/controller.js/controller.js
@@ -0,0 +1,9 @@
+const inventory = require('./data.json');
+
+module.exports = {
+    
+    getInventory: (req, res) => {
+
+        res.status(200).send(inventory)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-const inventory = require('./data.json')
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const port = 4000;
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`live on port${port}`))
+
